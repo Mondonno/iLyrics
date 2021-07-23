a1ea8e6f9c710a9bdf9770a47d90bab01cc5394b
Mondonno <mikolaj.mocek@icloud.com>
2021-07-23 11:01:25 +0200

    chore: Removed unused files, and other code instances

--- ilyrics/test/cutterTest.js
+++
@@ -1,8 +0,0 @@
-const ICustomizer = require("../src/modules/helpers/ICustomizer");
-const customizer = new ICustomizer();
-
-const m = async () => {
-    let c = customizer.customize("wtf <haha, ha, h, <cool>> kekw", "lol (with cutomizer)");
-    console.log(await c);
-}
-m();
